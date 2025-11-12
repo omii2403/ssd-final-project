88266f816ea30723ca7445c929ecd8b09a14cd3f
AnkitChavda023 <ankitchvada8479@gmail.com>
2025-11-13 00:54:50 +0530

    Added 2 more problem

---
+++ bug_portfolio/find_max_val_buggy.py
@@ -0,0 +1,8 @@
+import sys 
+def find_max_val(n, x, y): 
+	ans = -sys.maxsize 
+	for k in range(n + 1): 
+		if (k % x == y): 
+			ans = max(ans, k) 
+	return (ans if (ans > 0 and # bug fix: changed from >= 0 to > 0
+					ans <= n) else -1)
