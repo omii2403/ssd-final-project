cac2ac75714c3ffa2cddfaa43af7334d42280941
Hardik <hardikkothari300@gmail.com>
2025-11-12 22:24:15 +0530

    binomial_Coefficinet

---
+++ bug_portfolio/binomial_Coeff_buggy.py
@@ -0,0 +1,13 @@
+
+# "Write a python function to find sum of product of binomial co-efficients."
+
+def binomial_Coeff(n, k): 
+    C = [0] * (k + 1)
+    C[0] = 1  # nC0 is 1 
+    for i in range(1, n + 1):  
+        for j in range(min(i, k), -1, -1):  # BUG: -1 instead of 0
+            C[j] = C[j] + C[j - 1]
+    return C[k]
+
+def sum_Of_product(n): 
+    return binomial_Coeff(2 * n, n - 1)
